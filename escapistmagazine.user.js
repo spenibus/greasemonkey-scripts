a5e1354347d4f20fccf39a959b58221d710d9d8a
spenibus <git@spenibus.net>
2015-12-08 01:17:42 +0100

    escapist: 4 spaces

--- escapistmagazine.user.js
+++ escapistmagazine.user.js
@@ -3,7 +3,7 @@
 // @namespace   greasemonkey@spenibus
 // @include     http*://escapistmagazine.com/*
 // @include     http*://*.escapistmagazine.com/*
-// @version     20150504-0102
+// @version     20151208-0016
 // @require     spenibus-greasemonkey-lib.js
 // @grant       unsafeWindow
 // ==/UserScript==
@@ -21,79 +21,80 @@ var loc = document.location;
 /**************************************************************** video links */
 if(loc.pathname.substr(0,13) == '/videos/view/') {
 
-   window.addEventListener('load', function(e){
+    window.addEventListener('load', function(e){
 
-      // init box and css
-      SGL.css(''
-         +'#spenibus_videoLinks {'
-            +'display:table;'
-            +'white-space:nowrap;'
-            +'text-align:left;'
-         +'}'
-         +'#spenibus_videoLinks > * {'
-            +'display:table-row;'
-         +'}'
-         +'#spenibus_videoLinks > * > * {'
-            +'display:table-cell;'
-            +'padding:1px 2px;'
-         +'}'
-         // hide all cells except first column
-         +'#spenibus_videoLinks > div > div ~ div {'
-            +'display:none;'
-         +'}'
-         // hover: show cells
-         +':hover > #spenibus_videoLinks > div > div ~ div {'
-            +'display:table-cell;'
-         +'}'
-
-      );
-      var box = SGL.displayBox('spenibus_videoLinks');
-      box.set('ready');
-
-      // link description
-      var title = null;
-      title = document.title.match(/^(.*)$/i)[1];
-      title = title.replace(/[\\\/\|\*\?<>:"]/g, ' - '); // special chars
-      title = title.replace(/\s+/g, ' ');                // excessive spaces
+        // init box and css
+        SGL.css(''
+            +'#spenibus_videoLinks {'
+                +'display:table;'
+                +'white-space:nowrap;'
+                +'text-align:left;'
+            +'}'
+            +'#spenibus_videoLinks > * {'
+                +'display:table-row;'
+            +'}'
+            +'#spenibus_videoLinks > * > * {'
+                +'display:table-cell;'
+                +'padding:1px 2px;'
+            +'}'
+            // hide all cells except first column
+            +'#spenibus_videoLinks > div > div ~ div {'
+                +'display:none;'
+            +'}'
+            // hover: show cells
+            +':hover > #spenibus_videoLinks > div > div ~ div {'
+                +'display:table-cell;'
+            +'}'
+        );
+        var box = SGL.displayBox('spenibus_videoLinks');
+        box.set('ready');
 
 
-      // time
-      box.set('time');
-      var timeStr = '';
-
-      var timeSrc = document.getElementById('video_detail_header')
-         .childNodes[1].innerHTML.match(/^.*\| (.*)$/)[1];
-
-      if(timeSrc) {
-         //var timeStr = SGL.timeFormat('Y-m-d H:i:s O', Date.parse(timeSrc));
-         var timeStr = SGL.timeFormatUTC('Y-m-d H:i:s', Date.parse(timeSrc))+' GMT';
-      }
+        // link description
+        var title = null;
+        title = document.title.match(/^(.*)$/i)[1];
+        title = title.replace(/[\\\/\|\*\?<>:"]/g, ' - '); // special chars
+        title = title.replace(/\s+/g, ' ');                // excessive spaces
 
 
-      // get players
-      box.set('players');
-      var obj = null;
-      for(var i in unsafeWindow.imsVideo.players) {
-         obj = unsafeWindow.imsVideo.players[i];
-         break;
-      }
+        // time
+        box.set('time');
+        var timeStr = '';
+
+        var timeSrc = document
+            .getElementById('video_detail_header')
+            .childNodes[1].innerHTML.match(/^.*\| (.*)$/)[1];
+
+        if(timeSrc) {
+            //var timeStr = SGL.timeFormat('Y-m-d H:i:s O', Date.parse(timeSrc));
+            var timeStr = SGL.timeFormatUTC('Y-m-d H:i:s', Date.parse(timeSrc))+' GMT';
+        }
 
 
-      // video urls
-      box.set('urls');
-      var html = '';
-      for(var i of obj.playlist.src) {
-         html += ''
-            +'<div>'
-               +'<div>'+i.res+'</div>'
-               +'<div>'+i.type+'</div>'
-               +'<div><a href="'+i.src+'">TheEscapist-'+timeStr+' - '+title+'</a></div>'
-            +'</div>'
-      }
+        // get players
+        box.set('players');
+        var obj = null;
+        for(var i in unsafeWindow.imsVideo.players) {
+            obj = unsafeWindow.imsVideo.players[i];
+            break;
+        }
 
 
-      box.set(html);
+        // video urls
+        box.set('urls');
+        var html = '';
+        for(var i of obj.playlist.src) {
+            html += ''
+                +'<div>'
+                    +'<div>'+i.res+'</div>'
+                    +'<div>'+i.type+'</div>'
+                    +'<div><a href="'+i.src+'">TheEscapist-'+timeStr+' - '+title+'</a></div>'
+                +'</div>'
+        }
 
 
-   }, false);
+        box.set(html);
+
+
+    }, false);
 }
