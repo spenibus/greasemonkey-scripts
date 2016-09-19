b088bb3dcb3b68c425a4e17d10b5bc0b00a281d5
spenibus <git@spenibus.net>
2016-09-19 04:47:13 +0200

    twitch: compact datetime string

--- twitch.user.js
+++ twitch.user.js
@@ -617,12 +617,13 @@ function archives() {
     function timeFormat(timestamp) {
         if(timestamp) {
             var date = new Date(timestamp*1000);
-            return date.getFullYear()+'-'+
-                ('0'+(date.getMonth()+1)).slice(-2)+'-'+
-                ('0'+date.getDate()).slice(-2)+' '+
-                ('0'+date.getHours()).slice(-2)+'-'+
-                ('0'+date.getMinutes()).slice(-2)+'-'+
-                ('0'+date.getSeconds()).slice(-2);
+            return date.getFullYear()
+                +('0'+(date.getMonth()+1)).slice(-2)
+                +('0'+date.getDate()).slice(-2)
+                +'-'+
+                +('0'+date.getHours()).slice(-2)
+                +('0'+date.getMinutes()).slice(-2)
+                +('0'+date.getSeconds()).slice(-2);
         }
         return 0;
     }
