c63652a211bf939e2e3918f1ec7c67028860300d
spenibus <git@spenibus.net>
2018-02-23 23:13:00 +0100

    youtube: version number

--- youtube.user.js
+++ youtube.user.js
@@ -4,7 +4,7 @@
 // @updateURL   https://github.com/spenibus/greasemonkey-scripts/raw/master/youtube.user.js
 // @include     http*://youtube.com/*
 // @include     http*://*.youtube.com/*
-// @version     20180223-2205
+// @version     20180223-2212
 // @require     spenibus-greasemonkey-lib.js
 // @grant       unsafeWindow
 // @grant       GM_xmlhttpRequest
